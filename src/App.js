049bcbf55cf1c8acf7dee89ade9c68139e194de5
x3mer333 <x3mer333@gmail.com>
2025-02-11 16:30:36 +0700

    feat: create a simple React app
    A default React app was built
    and styled in a romantic theme for Valentine's Day (February 14th)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'; // Подключаем стили
+
+function App() {
+  return (
+    <div className="container">
+      <div className="hearts">❤️ ❤️ ❤️</div>
+      <h1>💖Люблю мою Сашечку-Кошечку :** 💖</h1>
+      <p className="subtitle">Welcome to React 🚀 with Love & Magic ✨</p>
+      <div className="hearts">💖 💕 💗</div>
+    </div>
+  );
+}
+
+export default App;
